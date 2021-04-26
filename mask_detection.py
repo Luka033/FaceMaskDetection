5b1e8e8f8917bd8f50ecb106e37c6211d24e947f
Nhi Dang <nhidangsd@gmail.com>
2021-04-26 15:05:00 -0700

    updated screen drawing

--- mask_detection.py
+++ mask_detection.py
@@ -3,6 +3,8 @@ import argparse
 import cv2
 import numpy as np
 from tflite_runtime.interpreter import Interpreter
+import colors as color
+import draw_utils as draw
 import time
 
 # Define and parse input arguments
@@ -46,7 +48,7 @@ input_mean = 127.5
 input_std = 127.5
 
 # Open video file
-video = cv2.VideoCapture(1)
+video = cv2.VideoCapture(0)
 imW = video.get(cv2.CAP_PROP_FRAME_WIDTH)
 imH = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
@@ -73,32 +75,34 @@ def inference(frame):
     classes = interpreter.get_tensor(output_details[1]['index'])[0]  # Class index of detected objects
     scores = interpreter.get_tensor(output_details[2]['index'])[0]  # Confidence of detected objects
 
-    if (scores[0] > min_conf_threshold) and (scores[0] <= 1.0):
-        # Get bounding box coordinates and draw box. Interpreter can return coordinates that are outside of image
-        # dimensions, need to force them to be within image using max() and min()
-        ymin = int(max(1, (boxes[0][0] * imH)))
-        xmin = int(max(1, (boxes[0][1] * imW)))
-        ymax = int(min(imH, (boxes[0][2] * imH)))
-        xmax = int(min(imW, (boxes[0][3] * imW)))
+    hub_color = color.white
+    res = -1
 
-        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (10, 255, 0), 4)
+    if (scores[0] > min_conf_threshold) and (scores[0] <= 1.0):
+
+        if object_name == 'no mask':
+            res = 0
+            hub_color = color.red
+        if object_name == 'mask':
+            res = 1
+            hub_color = color.green
 
         # Draw label
         object_name = labels[int(classes[0])]  # Look up object name from "labels" array using class index
         label = '%s: %d%%' % (object_name, int(scores[0] * 100))  # Example: 'person: 72%'
-        labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)  # Get font size
-        label_ymin = max(ymin, labelSize[1] + 10)  # Make sure not to draw label too close to top of window
-        cv2.rectangle(frame, (xmin, label_ymin - labelSize[1] - 10),
-                      (xmin + labelSize[0], label_ymin + baseLine - 10), (255, 255, 255),
-                      cv2.FILLED)  # Draw white box to put label text in
-        cv2.putText(frame, label, (xmin, label_ymin - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0),
-                    2)  # Draw label text
 
-        if object_name == 'no mask':
-            return 0
-        if object_name == 'mask':
-            return 1
-    return -1
+        # Get bounding box coordinates and draw box. Interpreter can return coordinates that are outside of image
+        # dimensions, need to force them to be within image using max() and min()
+        ymin = int(max(1, (boxes[0][0] * imH))) - 40
+        xmin = int(max(1, (boxes[0][1] * imW)))
+        ymax = int(min(imH, (boxes[0][2] * imH))) + 10
+        xmax = int(min(imW, (boxes[0][3] * imW)))
+
+        topLeft = xmin, ymin
+        bottomRight = xmax, ymax
+        draw.infoBoxLabel(frame, label, topLeft, bottomRight, hub_color)
+        
+    return res
 
 
 def turn_on_light(detection_bit):
@@ -113,22 +117,27 @@ def turn_on_light(detection_bit):
 LED_output = -1
 total_time = 0
 timer_start = time.time()
-while True:
+
+while video.isOpened():
     # Start timer (for calculating frame rate)
     t1 = cv2.getTickCount()
 
     # Acquire frame and resize to expected shape [1xHxWx3]
     _, frame = video.read()
+
+    # Draw frame rate in corner of frame
+    cv2.putText(frame, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, color.blue_green,
+                2, cv2.LINE_AA)
+    
+    draw.create_hud(frame, color=color.white)
+
+
     detection_bit = inference(frame)
     if detection_bit == 0 or 1:
         if total_time > 2.0 and detection_bit != LED_output:
             LED_output = detection_bit
             turn_on_light(detection_bit)
             timer_start = time.time()
-
-    # Draw frame rate in corner of frame
-    cv2.putText(frame, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0),
-                2, cv2.LINE_AA)
 
     # All the results have been drawn on the frame, so it's time to display it.
     cv2.imshow('Object detector', frame)
@@ -141,8 +150,10 @@ while True:
     timer_end = time.time()
     total_time = timer_end - timer_start
 
-    # Press 'q' to quit
-    if cv2.waitKey(1) == ord('q'):
+    # Listen the inpuyr from keyboard
+    k = cv2.waitKey(1)
+    # Exit if press 'q' or 'Esc' from keyboard
+    if k == ord('q') or k == 27:
         break
 
 # Clean up
